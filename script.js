2cd2242ecde7f8c828915630577402285d75ddc2
Matheus Souza <104939380+Matheussmz@users.noreply.github.com>
2024-04-07 19:32:07 -0400

    theme function

---
+++ script.js
@@ -0,0 +1,15 @@
+function themeMode () {
+    const html=document.documentElement
+    html.classList.toggle("light")
+
+    //selecionar a imagem
+    const img =document.querySelector("#profile img")
+    //fazer a troca da imagem
+    if (html.classList.contains("ligth")){
+    //se estiver no light altere a foto 
+        img.setAttribute("src","./assets/avatar-light.png")
+    }else{
+    //se ñ pode manter a padrão
+        img.setAttribute("src", "./fotos/Ellipse.png")
+    }
+}
