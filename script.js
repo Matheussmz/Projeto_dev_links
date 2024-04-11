2b0a6cf6c47990e8c1bdfb9e10c4a10b8c340078
Mat <mtsouza238@gmail.com>
2024-04-11 18:33:49 -0400

    Background

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function themeMode () {
     //fazer a troca da imagem
     if (html.classList.contains("ligth")){
     //se estiver no light altere a foto 
-        img.setAttribute("src","./assets/avatar-light.png")
+        img.setAttribute("src","./fotos/avatar_dark.png")
     }else{
     //se ñ pode manter a padrão
-        img.setAttribute("src", "./fotos/Ellipse.png")
+        img.setAttribute("src", "./fotos/avatar.png")
     }
 }
