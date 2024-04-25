a164455accffb325593ec103f4624a9294bcda72
Matheus Souza <104939380+Matheussmz@users.noreply.github.com>
2024-04-25 18:19:33 -0400

    alterando atributos como do alt na mudança de temas

--- script.js
+++ script.js
@@ -5,11 +5,18 @@ function themeMode () {
     //selecionar a imagem
     const img =document.querySelector("#profile img")
     //fazer a troca da imagem
-    if (html.classList.contains("ligth")){
+    if (html.classList.contains("light")){
     //se estiver no light altere a foto 
         img.setAttribute("src","./fotos/avatar_dark.png")
     }else{
     //se ñ pode manter a padrão
         img.setAttribute("src", "./fotos/avatar.png")
+    }
+
+
+    if (html.classList.contains("light")) {
+        img.alt = "Uma self com uma parede barnca no fundo, Matheus esta usando um oculos com armação preta com um filtro mais claro";
+    } else {
+        img.alt = "Uma self com uma parede barnca no fundo, Matheus esta usando um oculos com armação preta com um filtro mais dark";
     }
 }
